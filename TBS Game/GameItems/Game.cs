a6ee0172002a4f57c979b33ff2c4ff37d877ed34
Николай Харченко <kharchenko1991@outlook.com>
2018-07-19 23:52:00 +0300

    Добавьте файлы проекта.

---
+++ TBS Game/GameItems/Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TBS_Game.GameItems
+{
+    public  class Game
+    {
+        // members 
+
+    }
+}
